d9d757cd4869eb430d5ceaf46c38f954ed16e69c
M5-sich <m5.michael.sich@gmail.com>
2025-09-28 12:50:20 -0400

    Cleaned up main's assignment 1 tests and unnecessary file parsing. Also organized convert.h

--- convert.h
+++ convert.h
@@ -2,19 +2,24 @@
 #define CONVERT_H
 #include <stdint.h>
 
+//=====================================================================//
+// Assignment 2 Functions
+
+void oct_to_bin(const char *oct, char *out);
+void oct_to_hex(const char *oct, char *out);
+void hex_to_bin(const char *hex, char *out);
+
+void to_sign_mag(int n, char *out);
+void to_ones_complement(int n, char *out);
+void to_twos_complement(int n, char *out);
+
+//=====================================================================//
 // Assignment 1 Functions
 
 void div_convert(uint32_t n, int base, char *out);
 void sub_convert(uint32_t n, int base, char *out);
 void print_tables(uint32_t n);
 
-// Assignment 2 Functions
-void oct_to_bin(const char *oct, char *out);
-void oct_to_hex(const char *oct, char *out);
-void hex_to_bin(const char *hex, char *out);
-void to_sign_mag(int n, char *out);
-void to_ones_complement(int n, char *out);
-void to_twos_complement(int n, char *out);
 
 
 #endif
